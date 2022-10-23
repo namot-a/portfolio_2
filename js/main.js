6b4d147290cde22c1d685d93267f2feac5b340ac
namot <namot-a@example.com>
2022-10-24 01:32:11 +0800

    last

--- js/main.js
+++ js/main.js
@@ -9,14 +9,12 @@ if(navToggle){
         navMenu.classList.add('show-menu')
     })
 }
-
 /*===== MENU HIDDEN =====*/
 if(navClose){
     navClose.addEventListener('click', () =>{
         navMenu.classList.remove('show-menu')
     })
 }
-
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
@@ -25,19 +23,51 @@ const linkAction = () =>{
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
-
-
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 const scrollHeader = () =>{
     this.scrollY >= 50 ? header.classList.add('bg-header') 
                        : header.classList.remove('bg-header')
 }
 window.addEventListener('scroll', scrollHeader)
-
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
 
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
 
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
 /*=============== SHOW SCROLL UP ===============*/ 
-
-
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distancr: '60px',
+    duration: 2500,
+    delay: 400,
+})
+
+sr.reveal(`.home__data, .foooter__content, .foooter__logo, .foooter__description`)
+sr.reveal(`.home__tree-1`, {origin: 'left', delay:800})
+sr.reveal(`.home__tree-2`, {origin: 'right', delay:800}) 
+sr.reveal(`.home__img`, {delay:800}) 
+sr.reveal(`.categoty__card, .items__card`, {interval:100})  
+sr.reveal(`.about__img, .about__data, .foooter__tree-2`, {origin: 'left'}) 
+sr.reveal(`.party__imges, .party__data, .foooter__tree-1`, {origin: 'right'}) 
